5eb2e0152fc172cecd3d8d928209339e25bcdfff
Christian Amoakohene <camoakohene@datrixtechsolutions.com>
2025-03-21 12:13:16 +0000

    map and env.local

--- silverline-resource/app/contact/components/Map.tsx
+++ silverline-resource/app/contact/components/Map.tsx
@@ -4,7 +4,7 @@ const Map = () => {
   return (
     <div className="maximum-width">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3970.2198984430256!2d-0.2243872250139177!3d5.681323994300262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwNDAnNTIuOCJOIDDCsDEzJzE4LjUiVw!5e0!3m2!1sfr!2sng!4v1742463188662!5m2!1sfr!2sng"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3970.2198984430256!2d-0.2243872250139177!3d5.681323994300262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwNDAnNTIuOCJOIDDCsDEzJzE4LjUiVw!5e0!3m2!1sen!2sgh!4v1742559083763!5m2!1sen!2sgh"
         style={{ border: 0 }}
         allowFullScreen={true}
         loading="lazy"
